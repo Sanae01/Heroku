d9e552d93ed6c939c8044297eef23cde979acb80
Sanae01 <132098822+Sanae01@users.noreply.github.com>
2024-04-07 10:23:58 +0000

    app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict(s):
         return "Erreur de décodage JSON: {}".format(e), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host='0.0.0.0')
